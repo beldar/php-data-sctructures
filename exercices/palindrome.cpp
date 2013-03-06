742d91a7bbc4f491e3848db69b79701eae3bc980
Marti Planellas <beldar.cat@gmail.com>
2013-03-06 13:22:00 +0100

    Implementation in C++ of the PHP algorithm, this one got accepted! So the PHP algorithm works too.

--- exercices/palindrome.cpp
+++ exercices/palindrome.cpp
@@ -1,6 +1,72 @@
+/*
+ * PHP implementation for problem: http://www.spoj.com/problems/PALIN/
+ * Accepted! Time 1.79, Mem: 2.8M
+ * 
+A positive integer is called a palindrome if its representation in the decimal system is the same when read from left to right and from right to left. For a given positive integer K of not more than 1000000 digits, write the value of the smallest palindrome larger than K to output. Numbers are always displayed without leading zeros.
+
+Input
+
+The first line contains integer t, the number of test cases. Integers K are given in the next t lines.
+
+Output
+
+For each K, output the smallest palindrome larger than K.
+
+Example
+
+Input:
+2
+808
+2133
+Output:
+818
+2222
+ * 
+ * Difficult test cases:
+ * 7957 -> 7997
+ * 9789 -> 9889
+ * 1 -> 2
+ * 999 -> 1001
+ * 4599954 -> 4600064
+ * 45874 -> 45954
+ * 100100 -> 101101
+ * 94187978322 -> 94188088149
+ * 20899802 -> 20900902
+ * 
+ * ***
+ * 
+input : 
+10 
+3 
+9 
+53936 
+53236 
+67775 
+99 
+999 
+6339 
+875111 
+6997 
+
+output: 
+4 
+11 
+54045 
+53335 
+67776 
+101 
+1001 
+6446 
+875578 
+7007 
+ * @author Martí Planellas <beldar.cat at gmail >
+ * 
+ */
+
 #include <iostream> 
 #include <string>
 #include <cstdlib>
+#include <cstdio>
 #include <sstream>
 
 using namespace std; 
@@ -15,80 +81,113 @@ bool checknine(string str){
     }
     return isnine;
 }
+string getmirror(string str, int h, bool isodd, bool ls){
+    int n = str.length();
+    if(ls){
+        int piv = 0;
+        int h1 = h-1;
+        if(isodd)
+            piv=str[h];
+        else
+            piv=str[h1];
 
-string nextpal(string str){
-    istringstream ss(str);
-    ostringstream os;
-    if(str.length()==1 && str.compare("9")!=0){
-        int a;
-        ss >> a;
-        a++;
-        os << a;
-        return os.str();
-    }else{
-        if(checknine(str))
-            cout << "All nines!";
-
-    }
+        piv++;
+        if(!isodd)
+            str[h1] = piv;
+        else
+            str[h] = piv;
+        for(int i=n-1;i>=h;i--)
+            str[i] = str[n-i-1];
         return str;
         
-/*if(strlen($str)==1 && $str!="9")
-        return ++$str;
-    elseif(checknine($str)){
-        //print "Case all nines\n";
-        $str = str_replace ("9", "0", $str);
-        $str[0] = "1";
-        return $str."1";
     }else{
-        $n = strlen($str);
-        $h = floor(strlen($str)/2);
-        $isodd = ($n%2==1);
-        $i = $h-1;
-        $j = $isodd?$h+1:$h;
-        if($str[$i]!=$str[$j] && $str[$i]!=9 && $str[$j]!=9){
-            if($str[$i]>$str[$j]){
-                //print "Case left side largest\n";
-                return getmirror($str,$h,$isodd,false);
-            }elseif($str[$i]<$str[$j] && $str[$i]!=9 && $str[$j]!=9){
-                //print "Case right side largest\n";
-                return getmirror($str,$h,$isodd,true);
-            }
-        }else{
-            //print "Case more complex\n";
-            $ls = false;
-            while($i>0 && $str[$i]==$str[$j]){
-                $i--;
-                $j++;
-            }
-            if($i==0 || $str[$i]<$str[$j])
-                $ls = true;
-            if($i==0 && $str[$i]>$str[$j]){
-                $str[$j] = $str[$j]+1;
-                return $str;
-            }
-            if($ls){
-                $carry = 1;
-                $i=$h-1;
-                if($isodd){
-                    $aux = $str[$h]+$carry;
-                    $carry = $aux/10;
-                    $str[$h] = $aux%10;
-                    $j = $h+1;
-                }else
-                    $j = $h;
-                while($i>=0 && $carry!=0){
-                    $aux = $str[$i]+$carry;
-                    $carry = $aux/10;
-                    $str[$i] = $aux%10;
-                    $str[$j++] = $str[$i--];
-                }
-                return $str;
-            }else
-                return getmirror($str,$h,$isodd,false);
-                
-        }
-    }*/
+        if(isodd)
+            str[h+1] = str[h];
+        for(int i=n-1;i>=h;i--)
+            str[i] = str[n-i-1];
+        return str;
+    }
 }
+
+char *itoa(int value,char *buf) {
+    sprintf(buf, "%d", value);
+    return buf;
+}
+
+string nextpal(string str){
+    if(str.length()>1000000) return "";
+    if(str[str.length()-1]=='\n')
+        str.erase(str.length()-1);
+    ostringstream os;
+    int n = str.length();
+    if(n==1 && str.compare("9")!=0){
+        int a;
+        a=str[0];
+        a++;
+        str[0]=a;
+        return str;
+    }else{
+        if(checknine(str)){
+            os << "1";
+            for(int i=0;i<n;i++)
+                if(i==n-1) os << "1";
+                else os << "0";
+            return os.str();
+        }else{
+            int h = n/2;
+            bool isodd = (n%2==1);
+            int i = h-1;
+            int j = isodd ? h+1 : h;
+            if(str[i]!=str[j] && str[i]!='9' && str[i]!='9'){
+                if(str[i]>str[j])
+                    return getmirror(str,h,isodd,false);
+                else
+                    if(str[i]<str[j] && str[i]!='9' && str[i]!='9')
+                        return getmirror(str,h,isodd,true);
+                
+            }else{
+                bool ls = false;
+                while(i>0 && str[i]==str[j])
+                    i--,j++;
+                if(i==0 || str[i]<str[j])
+                    ls = true;
+                if(i==0 && str[i]>str[j]){
+                    int a = str[j];
+                    str[j] = ++a;
+                    return str;
+                }
+                if(ls){
+                    int carry = 1;
+                    int a=0;
+                    i = h-1;
+                    char b;
+                    if(isodd){
+                        b = str[h];
+                        a = atoi(&b);
+                        a += carry;
+                        carry = a/10;
+                        itoa(a%10,&b);
+                        str[h] = b;
+                        j = h+1;
+                    }else
+                        j = h;
+                    while(i>=0){
+                        b = str[i];
+                        a = atoi(&b);
+                        a += carry;
+                        carry = a/10;
+                        itoa(a%10,&b);
+                        str[i] = b;
+                        str[j++] = str[i--];
+                    }
+                    return str;
+                }else
+                    return getmirror(str,h,isodd,false);
+            }
+        }
+            
+    }
+}       
 
 int main(int argc, char *argv[]) 
 { 

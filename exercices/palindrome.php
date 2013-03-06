1272025f6b2c8d299a3b0da90b49028eff86664e
Marti Planellas <beldar.cat@gmail.com>
2013-03-06 12:28:55 +0100

    Add comments and clean up.

--- exercices/palindrome.php
+++ exercices/palindrome.php
@@ -60,21 +60,30 @@ output:
 6446 
 875578 
 7007 
+ * @author Martí Planellas <beldar.cat at gmail >
+ * 
  */
 $n = intval(fgets( STDIN ));
 $fifo = array();
+
 while($n>0){
     $in = fgets( STDIN );
     array_unshift($fifo,$in);
     $n--;
 }
+
 while($op = array_pop($fifo)){
     print nextpal(trim($op))."\n";
 }
+
 function nextpal($str){
+    //If offlimits return
     if(strlen($str)>1000000) return;
+    //If its a number < 10 and it's not 9 just increment and return
     if(strlen($str)==1 && $str!="9")
         return ++$str;
+    //Check if all numbers are 9, if they are, replace all with 0 put the first position 1 and add 1 at the end
+    // Example 999 -> 000 -> 100 -> 1001
     elseif(checknine($str)){
         //print "Case all nines\n";
         $str = str_replace ("9", "0", $str);
@@ -86,28 +95,38 @@ function nextpal($str){
         $isodd = ($n%2==1);
         $i = $h-1;
         $j = $isodd?$h+1:$h;
+        //If the center digits are different and not 9
         if($str[$i]!=$str[$j] && $str[$i]!=9 && $str[$j]!=9){
             if($str[$i]>$str[$j]){
-                //print "Case left side largest\n";
+                //Case the left side is larger
                 return getmirror($str,$h,$isodd,false);
             }elseif($str[$i]<$str[$j] && $str[$i]!=9 && $str[$j]!=9){
-                //print "Case right side largest\n";
+                //Case the right side is larger
                 return getmirror($str,$h,$isodd,true);
             }
         }else{
-            //print "Case more complex\n";
+            //If the center numbers are equal or there're 9s;
             $ls = false;
+            //If the center digits are equal, the number can be a palindrome
+            //Find at what point stops being a palindrome
             while($i>0 && $str[$i]==$str[$j]){
                 $i--;
                 $j++;
             }
+            //If it is a palindrome or the left side is smaller than right side ls=true
             if($i==0 || $str[$i]<$str[$j])
                 $ls = true;
+            //If it's a palindrome except for the first and last digit
+            // And the left digit is larger than the right, just copy the left digit to the right and return
+            // Example: 6772 -> 6776
             if($i==0 && $str[$i]>$str[$j]){
                 $str[$j] = $str[0];
                 return $str;
             }
             if($ls){
+                //The left side is smaller or number is already a palindrome
+                //We have to increment the middle digit and extend the carry both ways
+                //This covers the middle digits being 9s too
                 $carry = 1;
                 $i=$h-1;
                 if($isodd){
@@ -117,6 +136,7 @@ function nextpal($str){
                     $j = $h+1;
                 }else
                     $j = $h;
+                //Extend the carry both ways until we reach the end or we run out of carry
                 while($i>=0 && $carry!=0){
                     $aux = $str[$i]+$carry;
                     $carry = $aux/10;
@@ -125,6 +145,7 @@ function nextpal($str){
                 }
                 return $str;
             }else
+                //Case the left side is larger
                 return getmirror($str,$h,$isodd,false);
                 
         }
@@ -165,135 +186,4 @@ function checknine($str){
     }
     return $isnine;
 }
-/* Previous
- * function getmirror($str,$h,$isodd,$ls){
-    if($ls){
-        $fh = str_split($str, $h);
-        $fh = $fh[0];
-        $end = strlen($fh)-1;
-        $piv = intval($fh[$end]);
-        if($isodd)
-            $piv = intval($str[$h]);
-        $piv++;
-        if(!$isodd){
-            $fh[$end]=$piv;
-            $piv = "";
-        }
-    }else{
-        $piv = "";
-        $fh = str_split($str, $h);
-        $fh = $fh[0];
-        if($isodd)
-            $piv = $str[$h]; 
-    }
-    return $fh.$piv.strrev($fh);
-}*/
-/**
- * Version 4
- * function nextpal($str){
-    $half = ceil(strlen($str)/2)-1;
-    $fh = str_split($str, $half);
-    $fh = $fh[0];
-    $piv1 = intval($str[$half]);
-    $piv2 = intval($str[++$half]);
-    $in = false;
-    if($piv1==9 && $piv2!=9)
-        $piv = 9;
-    elseif($piv1==9 && $piv2==9){
-        $piv = 0;
-        $in = checknine($str);
-        if($in){
-            $fh = str_replace ("9", "0", $fh);
-            $fh[0] = "1";
-            $piv = "0";
-        }else{
-            $a = $fh[strlen($fh)-1];
-            $fh[strlen($fh)-1]=++$a;
-        }
-    }elseif($piv1>=$piv2){
-        $piv=$piv1;
-    }else
-        $piv = $piv1+1;
-    print $fh.$piv;
-    if(strlen($str)%2==0)
-        print $piv;
-    if($in) 
-        print $piv;
-    print strrev($fh)."\n";
-}
-
-function checknine($str){
-    $isnine = true;
-    for($i=0;$i<strlen($str);$i++){
-        if($str[$i]!="9"){
-            $isnine = false;
-            break;
-        }    
-    }
-    return $isnine;
-}*/
-/**
- * Version 3
-function nextpal($ints){
-    $r=array();
-    for($i=0;$i<ceil(count($ints)/2)-1;$i++){
-        $r[]=$ints[$i];
-        print $ints[$i];
-    }
-    $p=++$ints[$i];
-    print $p;
-    if(count($ints)%2==0)
-        print $p;
-    print implode("",$r)."\n";
-}*/
-/* Version 2
- * function nextpal($ints){
-    $parts = array_chunk($ints, ceil(count($ints)/2));
-    if(count($parts[0])>count($parts[1])){
-        $pivot = array_pop($parts[0]);
-        $rev = array_reverse($parts[0]);
-        $parts[0][]=++$pivot;
-        $r = array_merge($parts[0],$rev);
-    }else{
-        $pivot = array_pop($parts[0]);
-        $parts[0][]=++$pivot;
-        $rev = array_reverse($parts[0]);
-        $r = array_merge($parts[0],$rev);
-    }
-    unset($parts);
-    return $r;
-}*/
-/* Version 1
- * function nextpal($ints){
-    $r = false;
-    while(!$r && count($ints)<1000000){
-        $ints = increment($ints,count($ints)-1);
-        $i = 0;
-        $j = count($ints)-1;
-        $t = true;
-        while($i<$j && $t){
-            if($ints[$i]!=$ints[$j])
-                $t = false;
-            $i++;
-            $j--;
-        }
-        if($t)
-            $r = implode("",$ints);
-    }
-    return $r;
-}
-function increment($ints,$i){
-    if($ints[$i]==9){
-        $ints[$i] = 0;
-        if($i>0)
-            $ints = increment($ints,--$i);
-        else{
-            array_unshift($ints,0);
-            $ints = increment($ints,$i);
-        }
-            
-    }else
-        $ints[$i]++;
-    return $ints;
-}*/
 ?>
